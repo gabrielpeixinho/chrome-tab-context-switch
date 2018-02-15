ed0c9012cf0a98cfeaaf47f9e92aaeae16754ab8
Gabriel Peixinho <gabriel.peixinho@gmail.com>
2018-02-15 01:28:09 -0200

    adicionado jquery

--- popup.js
+++ popup.js
@@ -1,6 +1,22 @@
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-   // console.log("DOMContentLoaded");
-});
+var app = {
+
+   "init" : function( ){
+       var rmvs = $(".context-item");
+       
+       rmvs.each(function(){
+            var parent = this;
+            $('.rmv', this).click(function(){
+                parent.remove(); 
+            });
+       });
+
+
+   }
+};
+
+$(document).ready(function () {
+     app.init();
+})
